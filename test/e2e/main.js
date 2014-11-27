d9304d4da2d9c89c9ba9834d1460c86ffb218f04
Edouard <edou4rd@gmail.com>
2014-11-27 15:21:15 +0100

    tests

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -7,8 +7,9 @@ describe('The main view', function () {
   });
 
   it('list more than 5 awesome things', function () {
-    element.all(by.repeater('awesomeThing in awesomeThings')).count().then(function(count) {
-      expect(count > 5).toBeTruthy();
+
+    element.all(by.repeater('query in columns')).count().then(function(count) {
+      expect(count > 1).toBeTruthy();
     });
   });
 
