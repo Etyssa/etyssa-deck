7c7c8a43db410e3e151d52b37ec228ed5bada793
Edouard <edou4rd@gmail.com>
2014-12-14 22:49:05 +0100

    comments removed

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,6 @@
 
 angular.module('etyssaDeck',
   [ 'etyssaDeck.navbar',
-    // 'etyssa',
     'ngEtyssa.services',
     'ngStorage',
     'ngAnimate',
@@ -12,12 +11,7 @@ angular.module('etyssaDeck',
     'ui.router',
     'ui.bootstrap'
   ]
-)
-// .run(["api", function(api) {
-//     api.init({email:"olivoelement", password:"coucou"});
-// }]
-// )
-.config(['etyssaApiProvider', function(etyssaApiProvider) {
+).config(['etyssaApiProvider', function(etyssaApiProvider) {
   etyssaApiProvider.API_HOST = 'http://api.scriptysse.fr';
   etyssaApiProvider.TOKEN = 'a5ec51cb-8590-4a50-b797-9fbbaf50854a';
   etyssaApiProvider.EMAIL = 'olivoelement';
